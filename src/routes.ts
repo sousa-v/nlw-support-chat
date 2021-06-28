7e2c94cf248075881d5547fa1d0eb25f05906b07
Victor Brito de Sousa <victorbrito001@gmail.com>
2021-06-28 16:14:07 -0300

    create SetingsController

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express"
+
+import { SettingsController } from "./controllers/SettingsController"
+
+const routes = Router()
+
+const settigsController = new SettingsController()
+
+routes.post("/settings", settigsController.create)
+
+export { routes }
